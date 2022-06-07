a8688ef83dffd8e75dd0f9d661fac9876d2450e1
luk3mn <lucasrenan2015@gmail.com>
2022-06-06 21:24:34 -0300

    Criado tabela de ventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
+from turtle import title
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuario', 'data_evento',) # add um filtro, a virgula ',' precisa está no final
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) # no máximo 100 caracteres
+    descricao = models.TextField(blank=True, null=True) # pode ficar em branco e ser nulo
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True) # Inseri a hora atual automaticamente
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # se o usuario for deletado, seu evento tambem será deletado
+
+    class Meta:
+        db_table = 'evento' # exige que a tabela chaemo 'evento'
+
+    def __str__(self):
+        return self.titulo
